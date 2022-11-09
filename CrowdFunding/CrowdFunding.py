b486b7cf9143270f2f49b4e5e83b3605182c3c9c
TheMastro-11 <65859573+TheMastro-11@users.noreply.github.com>
2022-11-09 22:47:08 +0100

    Create CrowdFunding.py

---
+++ CrowdFunding/CrowdFunding.py
@@ -0,0 +1,104 @@
+import smartpy as sp
+
+class CrowdFunding(sp.Contract):
+    def __init__(self, deadline):
+        self.init(startDate = sp.timestamp(0), endDate = 1, contributors = sp.map(l = {}, tkey = sp.TAddress, tvalue = sp.TList(sp.TMutez) ), minAmount = sp.mutez(10), maxAmount = sp.mutez(1000), ceiling = sp.mutez(100000), floor = sp.mutez(200), isSuccess = sp.bool(False)  )
+
+    @sp.entry_point
+    def checkTime(self, time):
+        diffTime = time - self.data.startDate
+        sp.verify(diffTime <= self.getWeeks(), message = "The time is over")
+
+    def getWeeks(self):
+        return sp.compute(self.data.endDate * 86400)
+    
+    @sp.entry_point
+    def contribute(self):
+        #verifico che non sia stato raggiungo il tetto dei contributi
+        sp.verify(sp.balance + sp.amount <= self.data.ceiling, message = "Ceiling reached")   
+        #verifico che l'importo sia tra il minimo e massimo
+        sp.verify(sp.amount >= self.data.minAmount, message = "Amount too low")
+        sp.verify(sp.amount <= self.data.maxAmount, message = "Amount too high")
+        
+        #aggiungo nella lista
+        sp.if (self.data.contributors.contains(sp.sender)): 
+            self.data.contributors[sp.sender].push(sp.amount) #se esiste già
+        sp.else:
+            self.data.contributors[sp.sender] =  sp.list([sp.amount], t = sp.TMutez) #inserisco indirizzo contribuente
+    
+    
+    @sp.entry_point
+    def checkFloor(self):
+        sp.if sp.balance < self.data.floor:
+            addressList = sp.local("addressList", self.data.contributors.keys())
+            sp.for i in addressList.value:
+                with sp.match_cons(addressList.value) as x1:
+                    address = x1.head
+                    addressList.value = x1.tail
+                    sp.send(address, self.checkTotal(self.data.contributors[address]))
+        sp.else:
+            self.data.isSuccess = True
+    
+    def checkTotal(self, list_):
+        total = sp.local("totale", sp.mutez(0))
+        sp.for j in list_:
+            with sp.match_cons(list_) as x1:
+                total.value += x1.head
+                list_ = x1.tail
+        return total.value
+
+
+    @sp.entry_point
+    def endFunding(self, cAddress):
+        c = sp.contract(sp.TMap(sp.TAddress, sp.TList(sp.TMutez)), cAddress, entry_point = "airdrop").open_some()
+        sp.transfer(self.data.contributors, sp.balance, c)
+
+class TokenGen(sp.Contract):
+    def __init__(self):
+        self.init(supply = 120000000, contributors = sp.map(l = {}, tkey = sp.TAddress, tvalue = sp.TNat))
+
+    
+    @sp.entry_point
+    def airdrop(self, adMap):
+        adList = sp.local("adList", adMap.keys())
+        sp.for i in adList.value:
+            with sp.match_cons(adList.value) as x1:
+                address = x1.head
+                adList.value = x1.tail
+                self.data.contributors[address] = sp.utils.mutez_to_nat(self.checkTotal(adMap[address])) * 1200
+
+    def checkTotal(self, list_):
+        total = sp.local("totale", sp.mutez(0))
+        sp.for j in list_:
+            with sp.match_cons(list_) as x1:
+                total.value += x1.head
+                list_ = x1.tail
+        return total.value
+
+@sp.add_test(name = "Crowdfunding")
+def testCrowd():
+    #inizializzo scenario
+    scenario = sp.test_scenario()
+    #creo oggetto crowdfunding
+    crowdFunding = CrowdFunding(14)
+    #creo oggetto tokenGen
+    tokenGen = TokenGen()
+    #avvio scenario
+    scenario += crowdFunding
+    scenario += tokenGen
+
+    #creo users
+    pippo = sp.test_account("pippo")
+    sofia = sp.test_account("sofia")
+    sergio = sp.test_account("sergio")
+
+    time = sp.timestamp(0) #calcoliamo il tempo di esecuzione
+    time = time.add_hours(1)
+    crowdFunding.checkTime(time)
+    crowdFunding.contribute().run(sender = pippo, amount = sp.mutez(10))
+    crowdFunding.contribute().run(sender = sofia, amount = sp.mutez(100))
+    crowdFunding.contribute().run(sender = pippo, amount = sp.mutez(10))
+    crowdFunding.contribute().run(sender = sergio, amount = sp.mutez(1000))
+    crowdFunding.contribute().run(sender = sofia, amount = sp.mutez(10000)).run(valid = False)
+    crowdFunding.checkFloor()
+    crowdFunding.endFunding(tokenGen.address)
