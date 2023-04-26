a8cab6f986dd2bf0f488202b68ab59ef68b3f4a6
TheMastro-11 <matrevolv@icloud.com>
2023-04-26 15:45:16 +0200

    Update HTLC.py

--- contracts/HTLC/SmartPy/HTLC.py
+++ contracts/HTLC/SmartPy/HTLC.py
@@ -2,12 +2,11 @@ import smartpy as sp
 
 class HashTimedLockedContract(sp.Contract):
     def __init__(self):
-        self.init(collateral = sp.none, deadline = sp.none, commiter = sp.none , receiver = sp.none, hash = sp.none)
+        self.init(deadline = sp.none, commiter = sp.none , receiver = sp.none, hash = sp.none)
 
     @sp.entry_point
     def commission(self, deadline, receiver, hash):
         #save into data
-        self.data.collateral = sp.some(sp.amount)
         self.data.deadline = sp.some(sp.level + deadline)
         self.data.receiver = sp.some(receiver)
         self.data.hash = sp.some(hash)
@@ -22,20 +21,18 @@ class HashTimedLockedContract(sp.Contract):
         hash = sp.keccak(bytes) #created
         sp.verify(self.data.hash == sp.some(hash), "Wrong word") #checked
 
-        #check receiver
-        sp.verify(self.data.receiver == sp.some(sp.sender), "Wrong Address")
-        #transfer collateral to receiver
-        sp.send(self.data.receiver.open_some(), self.data.collateral.open_some())
+        #transfer collateral to commiter
+        sp.send(self.data.commiter.open_some(), sp.balance)
 
 
     @sp.entry_point
     def timeout(self):
         #check if deadline is reached and if sender is the commiter
         sp.verify(self.data.deadline <= sp.some(sp.level), "Deadline not reached")
-        sp.verify(self.data.commiter == sp.some(sp.sender), "You're not the commiter")
+        sp.verify(self.data.receiver == sp.some(sp.sender), "You're not the receiver")
 
-        #transfer collateral to commiter
-        sp.send(self.data.commiter.open_some(), self.data.collateral.open_some())
+        #transfer collateral to receiver
+        sp.send(self.data.receiver.open_some(), sp.balance)
 
         
     
@@ -58,11 +55,11 @@ def testHTLC():
     bytes = sp.pack(secret)
     hash = sp.keccak(bytes)
     #first commission
-    htlc.commission(sp.record(deadline = sp.nat(10) , receiver = sofia.address, hash = hash)).run(sender = pippo, amount = sp.tez(1000))
+    htlc.commission(sp.record(deadline = sp.nat(10) , receiver = sofia.address, hash = hash)).run(sender = pippo, amount = sp.mutez(1000))
     #reveal after 50 rounds
-    htlc.reveal("love").run(sender = sofia)
+    htlc.reveal("love").run(sender = pippo)
     #timeout after 100 rounds
-    htlc.timeout().run(sender = pippo, level = 1000)
+    htlc.timeout().run(sender = sofia, level = 1000)
 
 
 
